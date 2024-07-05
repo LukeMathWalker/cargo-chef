a1b86d4493b39fa4e096f8b6f5c4e283003c5531
somehowchris <christof@weickhardt.ch>
2024-07-05 16:23:27 +0200

    chore: add profile max-perf-at-small-bin

--- src/skeleton/mod.rs
+++ src/skeleton/mod.rs
@@ -158,14 +158,11 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
             let parsed_manifest =
                 cargo_manifest::Manifest::from_slice(manifest.contents.as_bytes())?;
 
-            let is_harness = |products: &Option<Vec<Product>>, name: &str| -> bool {
+            let is_harness = |products: &Vec<Product>, name: &str| -> bool {
                 products
-                    .as_ref()
-                    .and_then(|v| {
-                        v.iter()
-                            .find(|product| product.name.as_deref() == Some(name))
-                            .map(|p| p.harness)
-                    })
+                    .iter()
+                    .find(|product| product.name.as_deref() == Some(name))
+                    .map(|p| p.harness)
                     .unwrap_or(true)
             };
 
@@ -243,7 +240,7 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
             if let Some(package) = parsed_manifest.package.as_ref() {
                 for target_directory in &target_directories {
                     // Remove dummy libraries.
-                    for lib in &parsed_manifest.lib {
+                    if let Some(lib) = &parsed_manifest.lib {
                         let library_name =
                             lib.name.as_ref().unwrap_or(&package.name).replace('-', "_");
                         let walker = GlobWalkerBuilder::from_patterns(
