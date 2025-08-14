719d31354a2a61c7a60908dcd7e0f4d237f4940f
Michel Heily <michel.heily@wiz.io>
2025-08-14 23:15:16 +0300

    Add support for --workspace --exclude

--- src/main.rs
+++ src/main.rs
@@ -121,6 +121,9 @@ pub struct Cook {
     /// Build all members in the workspace.
     #[arg(long)]
     workspace: bool,
+    /// Exclude packages from the build
+    #[arg(long, requires = "workspace")]
+    exclude: Option<Vec<String>>,
     /// Build offline.
     #[arg(long)]
     offline: bool,
@@ -185,6 +188,7 @@ fn _main() -> Result<(), anyhow::Error> {
             manifest_path,
             package,
             workspace,
+            exclude,
             offline,
             frozen,
             locked,
@@ -286,6 +290,7 @@ fn _main() -> Result<(), anyhow::Error> {
                     manifest_path,
                     package,
                     workspace,
+                    exclude,
                     offline,
                     timings,
                     no_std,
